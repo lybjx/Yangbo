7b7412b35e7fb7c43e5c2ac6bfa4122c7c5cf17e
unknown <david@mesh-studio.com>
2018-08-22 17:36:00 +0800

    change slider component

--- html/js/script.js
+++ html/js/script.js
@@ -3,8 +3,8 @@ $(function() {
 	var h = w/3;
 	$('#slider').attr('width', w);
 	$('#slider').attr('height', h);
-	$('.rslides img').attr('width', w);
-	$('.rslides img').attr('height', h);
+	$('.flash_slides img').attr('width', w);
+	$('.flash_slides img').attr('height', h);
 	$('.logo').css({top:0,left:361/1920*w});
 	$('.navi_bar').css({top:0,left:483/1920*w});
 	$('.login').css({top:0,left:1500/1920*w});
@@ -12,14 +12,20 @@ $(function() {
 	$('.intro').css({top:877/1920*w,left:663/1920*w});
 	$('.intro_head').css({top:869/1920*w,left:556/1920*w});
 	$('.intro_tail').css({top:869/1920*w,left:1331/1920*w});
+	$('.school_content').css({top:1017/1920*w,left:360/1920*w});
+	$('.school_content_more').css({top:1357/1920*w,left:896/1920*w});
 	$('.menu2').css({top:1595/1920*w,left:764/1920*w});
 	$('.foot').css({top:6340/1920*w,left:0});
-	$("#dowebok").slick({
+	$(".flash_slides").slick({
 		dots: true,
 		infinite: true,
-		// centerMode: true,
-		// slidesToShow: 1,
-		// slidesToScroll: 1
+		autoplay: true
+	});
+	$(".school_slides").slick({
+		dots: true,
+		infinite: true,
+		slidesToShow: 4,
+		// autoplay: true
 	});
 	// $('#dowebok').responsiveSlides({
 	// 	pager: true,
