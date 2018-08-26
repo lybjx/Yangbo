7897cb5ab8960453b576f5822625e2cb333b82fe
unknown <david@mesh-studio.com>
2018-08-26 20:51:34 +0800

    index.html update

--- html/js/script.js
+++ html/js/script.js
@@ -15,6 +15,19 @@ $(function() {
 	$('.school_content').css({top:1017/1920*w,left:360/1920*w});
 	$('.school_content_more').css({top:1357/1920*w,left:896/1920*w});
 	$('.menu2').css({top:1595/1920*w,left:764/1920*w});
+	$('.student_content').css({top:1669/1920*w,left:360/1920*w});
+	$('.student_content_more').css({top:2242/1920*w,left:896/1920*w});
+	$('.announce_content').css({top:2480/1920*w,left:360/1920*w});
+	$('.enrolment_content').css({top:3220/1920*w,left:0/1920*w});
+	$('.media_center_title').css({top:4119/1920*w,left:864/1920*w});
+	$('.media_center_english').css({top:4174/1920*w,left:848/1920*w});
+	$('.media_content').css({top:4270/1920*w,left:360/1920*w});
+	$('.media_content_more').css({top:4659/1920*w,left:896/1920*w});
+	$('.home_title').css({top:4867/1920*w,left:864/1920*w});
+	$('.home_english').css({top:4922/1920*w,left:851/1920*w});
+	$('.home_content').css({top:5027/1920*w,left:360/1920*w});
+	$('.home_content_more').css({top:5706/1920*w,left:896/1920*w});
+	$('.site_map_content').css({top:5854/1920*w,left:0/1920*w,width:w,height:487/1920*w});
 	$('.foot').css({top:6340/1920*w,left:0});
 	$(".flash_slides").slick({
 		dots: true,
@@ -25,8 +38,15 @@ $(function() {
 		dots: true,
 		infinite: true,
 		slidesToShow: 4,
-		// autoplay: true
+		autoplay: true
 	});
+	$(".media_slide").slick({
+		dots: true,
+		infinite: true,
+		slidesToShow: 3,
+		autoplay: true
+	});
+	$('.media_play_button').css({display:none});
 	// $('#dowebok').responsiveSlides({
 	// 	pager: true,
 	// 	nav: true,
